16f6da1516a0a5816a403308b00abe2ff1291b09
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-03 09:37:47 +0100

    PHP1Ex4

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name='Pauline';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>PHP1Ex4</title>
+</head>
+<body>
+  <p>Bonjour <?php echo $name;?>, comment vas-tu ?</p>
+</body>
+</html>
